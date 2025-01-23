d948266c1a7dc7e4c9066f996a63fba9c41993cd
Reemolke <riquelmeraj@gmail.com>
2025-01-23 12:35:09 +0100

    Ajustes css underline font size

--- Practica2/practica2/src/components/tarea.js
+++ Practica2/practica2/src/components/tarea.js
@@ -2,7 +2,7 @@ import React from "react";
 function Tarea({tarea,completada,index,eliminarTarea,completarTarea}){
     return (
         <div class="listaTexto">
-            <div class="texto"><span>{tarea}</span><span>{completada && (<img class="checked" src="https://static.vecteezy.com/system/resources/previews/010/152/436/original/tick-check-mark-icon-sign-symbol-design-free-png.png" alt="check"></img>)}
+            <div class="texto"><span>{completada && <em style={{textDecoration : 'line-through'}}>{tarea}</em>}{!completada && tarea}</span><span>{completada && (<img class="checked" src="https://static.vecteezy.com/system/resources/previews/010/152/436/original/tick-check-mark-icon-sign-symbol-design-free-png.png" alt="check"></img>)}
             {!completada && (<img class="checked" src="https://pngimg.com/d/hourglass_PNG21.png" alt="check"></img>)}</span></div>
             
             <div class="botones">
