2d8d682b60e9b1abf5db121b4ab8f3d23b17a0dc
Reemolke <riquelmeraj@gmail.com>
2025-01-23 10:12:16 +0100

    CSS pa que quede bonito

--- Practica2/practica2/src/components/tarea.js
+++ Practica2/practica2/src/components/tarea.js
@@ -2,8 +2,8 @@ import React from "react";
 function Tarea({tarea,completada,index,eliminarTarea,completarTarea}){
     return (
         <div class="listaTexto">
-            <div class="texto"><span>{tarea}{completada && (<img class="checked" src="https://d1nhio0ox7pgb.cloudfront.net/_img/v_collection_png/32x32/shadow/check.png" alt="check"></img>)}
-            {!completada && (<img class="checked" src="https://d1nhio0ox7pgb.cloudfront.net/_img/o_collection_png/green_dark_grey/16x16/plain/hourglass.png" alt="check"></img>)}</span></div>
+            <div class="texto"><span>{tarea}</span><span>{completada && (<img class="checked" src="https://static.vecteezy.com/system/resources/previews/010/152/436/original/tick-check-mark-icon-sign-symbol-design-free-png.png" alt="check"></img>)}
+            {!completada && (<img class="checked" src="https://pngimg.com/d/hourglass_PNG21.png" alt="check"></img>)}</span></div>
             
             <div class="botones">
                 <button onClick={()=>eliminarTarea(index)}>Eliminar</button>
