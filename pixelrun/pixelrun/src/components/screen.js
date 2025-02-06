f3c923364bf8b894a482b423cb50d27468df3e98
Reemolke <riquelmeraj@gmail.com>
2025-02-06 18:08:50 +0100

    Pantalla y dpad

--- pixelrun/pixelrun/src/components/screen.js
+++ pixelrun/pixelrun/src/components/screen.js
@@ -3,7 +3,17 @@ import React from 'react';
 function Screen(pagina){
     return(
     <div className='pantalla'>
+        
+            <div className='visualizacion'>
 
+            </div>
+            <div className='resumen'>
+
+            </div>
+            <div className='lista'>
+
+            </div>
+        
     </div>
     );
 }
