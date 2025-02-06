a79b44b13b9e0ba573058a67d7a427fc57744405
Reemolke <riquelmeraj@gmail.com>
2025-02-06 17:08:55 +0100

    Functional omni

--- pixelrun/pixelrun/src/components/stick.js
+++ pixelrun/pixelrun/src/components/stick.js
@@ -130,7 +130,7 @@ const Stick = () => {
           top: `${position.y}%`,
           transform: 'translate(-50%, -50%)',
           borderRadius: '50%',
-          backgroundColor: 'blue', // Color de la bola
+          backgroundColor: 'red', // Color de la bola
           cursor: 'pointer',
         }}
       />
