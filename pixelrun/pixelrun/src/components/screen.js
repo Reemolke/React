73a1194ec31a153bf93852cd0eed27109b42a9fe
Reemolke <riquelmeraj@gmail.com>
2025-02-06 20:06:50 +0100

    Ditto prueba

--- pixelrun/pixelrun/src/components/screen.js
+++ pixelrun/pixelrun/src/components/screen.js
@@ -5,10 +5,22 @@ function Screen(pagina){
     <div className='pantalla'>
         
             <div className='visualizacion'>
-
+            <h1>Pokemon</h1>
+            <div>
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/132.gif' alt='Pokemon'></img>
+            </div>
+            <p>Habilidad: </p>
+            <p>Descripción:</p>
             </div>
             <div className='resumen'>
-
+            <ul>Estadísticas
+                <li>ATQ</li>
+                <li>DEF</li>
+                <li>HP</li>
+                <li>ATQ E</li>
+                <li>DEF E</li>
+                <li>SPD</li>
+            </ul>
             </div>
             <div className='lista'>
 
